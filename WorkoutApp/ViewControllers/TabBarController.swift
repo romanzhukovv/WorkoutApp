7c7b9dc5c0b12d969961b9862a9620d3e4fb418a
romanzhukovv <zhukov.roman89@gmail.com>
2022-10-19 21:13:51 +0400

    refactoring

--- WorkoutApp/ViewControllers/TabBarController.swift
+++ WorkoutApp/ViewControllers/TabBarController.swift
@@ -18,6 +18,7 @@ final class TabBarControler: UITabBarController {
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
         configure()
     }
     
