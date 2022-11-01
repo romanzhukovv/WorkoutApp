7ea8423f131116346185df93edd033c5c152724e
romanzhukovv <zhukov.roman89@gmail.com>
2022-11-01 15:01:11 +0400

    added calendar to navBar

---
+++ WorkoutApp/ViewControllers/Overview/Views/WeekView/WeekDayView.swift
@@ -0,0 +1,61 @@
+//
+//  WeekDayView.swift
+//  WorkoutApp
+//
+//  Created by Roman Zhukov on 01.11.2022.
+//
+
+import UIKit
+
+extension WeekView {
+    final class WeekDayView: BaseView {
+        
+        private let nameLabel = UILabel()
+        private let dateLabel = UILabel()
+        private let stackView = UIStackView()
+        
+        func configure(with index: Int, and name: String) {
+            nameLabel.text = name.uppercased()
+            dateLabel.text = "\(index)"
+        }
+    }
+}
+
+extension WeekView.WeekDayView {
+    override func addViews() {
+        super.addViews()
+        
+        addView(stackView)
+        
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(dateLabel)
+    }
+    
+    override func layoutViews() {
+        super.layoutViews()
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        
+        backgroundColor = .red
+        
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+        
+        nameLabel.font = Resources.Fonts.helveticaRegular(with: 9)
+        nameLabel.textAlignment = .center
+        
+        dateLabel.font = Resources.Fonts.helveticaRegular(with: 15)
+        dateLabel.textAlignment = .center
+        
+        stackView.spacing = 3
+        stackView.axis = .vertical
+    }
+}
+
--- WorkoutApp/ViewControllers/Overview/Views/WeekView/WeekView.swift
+++ WorkoutApp/ViewControllers/Overview/Views/WeekView/WeekView.swift
@@ -9,18 +9,41 @@ import UIKit
 
 final class WeekView: BaseView {
     
+    private let calendar = Calendar.current
+    private let stackView = UIStackView()
 }
 
 extension WeekView {
     override func addViews() {
         super.addViews()
+        
+        addView(stackView)
     }
     
     override func layoutViews() {
         super.layoutViews()
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
     }
     
     override func configureViews() {
         super.configureViews()
+        
+        stackView.spacing = 7
+        stackView.distribution = .fillEqually
+        
+        let weekdays = calendar.shortStandaloneWeekdaySymbols
+        
+        weekdays.enumerated().forEach { index, name in
+            let view = WeekDayView()
+            view.configure(with: index, and: name)
+            stackView.addArrangedSubview(view)
+        }
+        
     }
 }
