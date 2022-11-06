abed36cf320cc5aa1d9487ab6b9cd39c77f8011b
romanzhukovv <zhukov.roman89@gmail.com>
2022-11-06 18:19:53 +0400

    added base info view

---
+++ WorkoutApp/ViewControllers/Base/BaseInfoView.swift
@@ -0,0 +1,71 @@
+//
+//  BaseInfoView.swift
+//  WorkoutApp
+//
+//  Created by Roman Zhukov on 06.11.2022.
+//
+
+import UIKit
+
+class BaseInfoView: BaseView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = Resources.Fonts.helveticaRegular(with: 12)
+        label.textColor = Resources.Colors.inactive
+        return label
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.borderColor = Resources.Colors.separator.cgColor
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    
+    init(with title: String? = nil, alignment: NSTextAlignment = .left) {
+        titleLabel.text = title?.uppercased()
+        titleLabel.textAlignment = alignment
+        
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BaseInfoView {
+    override func addViews() {
+        super.addViews()
+        
+        addView(titleLabel)
+        addView(contentView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let contentViewTopAnchor: NSLayoutAnchor = titleLabel.text == nil ? topAnchor : titleLabel.bottomAnchor
+        
+        let contentViewOffset: CGFloat = titleLabel.text == nil ? 0 : 10
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: contentViewTopAnchor, constant: contentViewOffset),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+        
+        backgroundColor = .clear
+    }
+}
--- WorkoutApp/ViewControllers/Session/SessionViewController.swift
+++ WorkoutApp/ViewControllers/Session/SessionViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class SessionViewController: BaseController {
+    
+    private let timerView: BaseInfoView = {
+        let view = BaseInfoView(with: "Timer", alignment: .center)
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,4 +25,27 @@ class SessionViewController: BaseController {
         addNavBarButton(at: .right, with: Resources.Strings.Session.navBarRight)
     }
 
+}
+
+extension SessionViewController {
+    override func addViews() {
+        super.addViews()
+        
+        view.addView(timerView)
+    }
+    
+    override func layoutViews() {
+        super.layoutViews()
+        
+        NSLayoutConstraint.activate([
+            timerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            timerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            timerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            timerView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+    
+    override func configure() {
+        super.configure()
+    }
 }
