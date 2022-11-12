49e84567717acf2ca71a063050d9e32e7a8ce50f
romanzhukovv <zhukov.roman89@gmail.com>
2022-11-12 23:17:28 +0400

    add class for TimerView

---
+++ WorkoutApp/ViewControllers/Session/Views/TimerView.swift
@@ -0,0 +1,26 @@
+//
+//  TimerView.swift
+//  WorkoutApp
+//
+//  Created by Roman Zhukov on 12.11.2022.
+//
+
+import UIKit
+
+final class TimerView: WABaseInfoView {
+    
+}
+
+extension TimerView {
+    override func addViews() {
+        super.addViews()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func configureViews() {
+        super.configureViews()
+    }
+}
